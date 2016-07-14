1ceaa48057f4794d3b08db4995927ab8d2fcb503
javaDer <fa20091001@163.com>
2016-07-14 13:46:47 +0800

    stumansys

---
+++ src/main/java/com/biyou/Action/Application.java
@@ -0,0 +1,21 @@
+package com.biyou.Action;
+
+import com.biyou.Action.UserController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * <p>User: Zhang Kaitao
+ * <p>Date: 13-12-22
+ * <p>Version: 1.0
+ */
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
---
+++ src/main/java/com/biyou/Action/UserController.java
@@ -0,0 +1,22 @@
+package com.biyou.Action;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.biyou.Pojo.User;
+
+//@EnableAutoConfiguration
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @RequestMapping("/{id}")
+    public User view(@PathVariable("id") Long id) {
+        User user = new User();
+        user.setId(id);
+        user.setName("张三");
+        return user;
+    }
+}
